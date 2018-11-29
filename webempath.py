43c52b66a1648f33534d6ce0071549f5762c7a5e
Ranjani Murali <43966472+rmurali9@users.noreply.github.com>
2018-11-29 15:03:42 -0500

    Create webempath.py

---
+++ webempath.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+import execjs
+ctx = execjs.compile(""" 
+var fs = require('fs');
+var request = require('request');
+
+function analyzeWav(apiKey, wavFilename) {
+  var url = 'https://hogehoge_server/v2/analyzeWav';
+  var formData = {
+    apikey: apiKey,
+    wav: fs.createReadStream(wavFilename)
+  };
+
+  request.post({ url: url, formData: formData }, function(err, response) {
+    if (err) {
+      console.trace(err);
+      return;
+    } else if (!response.body) {
+      console.trace("no response body");
+      return;
+    }
+
+    var result = JSON.parse(response.body);
+    console.log("result: " + JSON.stringify(result));
+  });
+}
+""")
+
+ctx.call("analyzeWav", 'GGMjyLNHsT37ny5CE8J_2fGIF5kiEbruyNoIuCAD9hk','C:/Users/aata/Desktop/output10.wav')
+
+
